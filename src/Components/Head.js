2f80bc3bf848b43abff3c6d28ae17cabba6d5d14
Yawar <rehaanyawer57@gmail.com>
2024-01-17 23:37:26 +0530

    Header

--- src/Components/Head.js
+++ src/Components/Head.js
@@ -1,7 +1,35 @@
 import React from 'react';
 
 const Head = () => {
-  return <div>Header Component</div>;
+  return (
+    <div className='grid grid-flow-col p-5 m-2 shadow-xl'>
+      <div className='flex col-span-1 bg-slate-300'>
+        <img
+          className='h-12'
+          src='https://cdn.iconscout.com/icon/free/png-256/free-hamburger-menu-462145.png'
+          alt='Menu'
+        />
+
+        <img
+          className='h-12'
+          src='https://cdn.mos.cms.futurecdn.net/8gzcr6RpGStvZFA2qRt4v6.jpg'
+          alt='Logo'
+        />
+      </div>
+
+      <div className=''>
+        <input type='text' border border-slate-600 />
+        <button className='border border-slate-600'>Search</button>
+      </div>
+      <div>
+        <img
+          className='h-12'
+          src='https://png.pngtree.com/png-vector/20191110/ourmid/pngtree-avatar-icon-profile-icon-member-login-vector-isolated-png-image_1978396.jpg'
+          alt='Proflile Icon'
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Head;
