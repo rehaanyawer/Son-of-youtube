bd195ffc3c56e66fa4430c5288b8776749041fad
Yawar <rehaanyawer57@gmail.com>
2024-01-18 20:40:01 +0530

    created Sidebar

--- src/Components/Head.js
+++ src/Components/Head.js
@@ -1,27 +1,40 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { toggleMenu } from './utils/appSlice';
 
 const Head = () => {
+  const dispatch = useDispatch();
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  };
   return (
-    <div className='grid grid-flow-col p-5 m-2 shadow-xl'>
-      <div className='flex col-span-1 bg-slate-300'>
+    <div className='grid grid-flow-col p-1 m-1 shadow-xl items-center'>
+      <div className='flex col-span-1 items-center'>
         <img
-          className='h-12'
+          onClick={() => toggleMenuHandler()}
+          className='h-12 p-2 cursor-pointer'
           src='https://cdn.iconscout.com/icon/free/png-256/free-hamburger-menu-462145.png'
           alt='Menu'
         />
 
         <img
-          className='h-12'
+          className='h-20 '
           src='https://cdn.mos.cms.futurecdn.net/8gzcr6RpGStvZFA2qRt4v6.jpg'
           alt='Logo'
         />
       </div>
 
-      <div className=''>
-        <input type='text' border border-slate-600 />
-        <button className='border border-slate-600'>Search</button>
+      <div className='col-span-10 flex justify-center'>
+        <input
+          className='w-2/5 border border-slate-300 rounded-l-full h-10 p-5 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500'
+          placeholder='Search'
+          type='text'
+        />
+        <button className='border p-2 px-4 bg-gray-100 hover:bg-gray-200 border-gray-300 rounded-r-full'>
+          Search
+        </button>
       </div>
-      <div>
+      <div className='col-span-1'>
         <img
           className='h-12'
           src='https://png.pngtree.com/png-vector/20191110/ourmid/pngtree-avatar-icon-profile-icon-member-login-vector-isolated-png-image_1978396.jpg'
--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const SideBar = () => {
-  return <div>SideBar</div>;
+  const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
+
+  if (!isMenuOpen) return null;
+  return (
+    <div className=' px-8 p-4 shadow-xl'>
+      <ul className=''>
+        <li>Home</li>
+        <li>Shorts</li>
+        <li>Subscriptions</li>
+      </ul>
+      <div className='w-[100%] border border-b-gray-300 my-2'></div>
+      <h2 className='font-bold py-2'>Explore</h2>
+      <ul>
+        <li>Trending</li>
+        <li>Shopping</li>
+        <li>Music</li>
+        <li>Films</li>
+        <li>Live</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+        <li>News</li>
+      </ul>
+      <div className='w-[100%] border border-b-gray-300 my-2'></div>
+      <h2 className='font-bold py-2'>More From Youtube</h2>
+      <ul>
+        <li>Trending</li>
+        <li>Shopping</li>
+        <li>Music</li>
+        <li>Films</li>
+        <li>Live</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+        <li>News</li>
+      </ul>
+    </div>
+  );
 };
 
 export default SideBar;
