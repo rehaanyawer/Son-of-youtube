6d062a00abc2488ca00558f9edaa4736a0e9da83
Yawar <rehaanyawer57@gmail.com>
2024-01-18 23:55:35 +0530

    Initializing API

--- src/Components/ButtonList.js
+++ src/Components/ButtonList.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const ButtonList = () => {
-  return <div>ButtonList</div>;
+const ButtonList = ({ buttonValue }) => {
+  return (
+    <div>
+      <button className='border p-1 px-3 m-4 bg-gray-100 rounded-lg hover:bg-gray-200 active:bg-black active:text-white'>
+        {buttonValue}
+      </button>
+    </div>
+  );
 };
 
 export default ButtonList;
--- src/Components/MainContainer.js
+++ src/Components/MainContainer.js
@@ -2,10 +2,29 @@ import React from 'react';
 import ButtonList from './ButtonList';
 import VideoContainer from './VideoContainer';
 const MainContainer = () => {
+  const btnArray = [
+    'All',
+    'Javascript',
+    'Gaming',
+    'Action',
+    'Comedy',
+    'Drama',
+    'Romance',
+    'Trailers',
+    'Gadgets',
+    'News',
+    'Live',
+  ];
   return (
     <div>
-      <ButtonList />
-      <VideoContainer />
+      <div className='flex'>
+        {btnArray.map((buttonValue) => {
+          return <ButtonList buttonValue={buttonValue} />;
+        })}
+      </div>
+      <div className=''>
+        <VideoContainer />
+      </div>
     </div>
   );
 };
