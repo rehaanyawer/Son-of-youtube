a31822416217dc87d0a871b724c291bac2049bd7
Yawar <rehaanyawer57@gmail.com>
2024-01-19 07:08:30 +0530

    fixed api key

--- src/Components/VideoContainer.js
+++ src/Components/VideoContainer.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const VideoContainer = () => {
-  return <div>VideoContainer</div>;
+  useEffect(() => {
+    const data = fetch(
+      'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=' +
+        process.env.REACT_APP_NEWS_API
+    );
+    const json = data.json;
+    console.log(json);
+  }, []);
+  return (
+    <div>
+      VideoContainer
+      {process.env.REACT_APP_NEWS_API}
+    </div>
+  );
 };
 
 export default VideoContainer;
--- src/Components/utils/Constants.js
+++
@@ -1,4 +0,0 @@
-export const API_KEY = 'AIzaSyB9KEFJFlmM7bj9YyXuUlfi_HyYGPF5hno';
-export const API_LINK =
-  'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=' +
-  API_KEY;
