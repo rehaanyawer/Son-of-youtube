fe80fc8616470c2af325bded7f7609304766998f
Yawar <rehaanyawer57@gmail.com>
2024-01-21 15:07:13 +0530

    debouncing

--- src/Components/Head.js
+++ src/Components/Head.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { toggleMenu } from './utils/appSlice';
+import { YOUTUBE_SEARCH_SUGGESTION } from './utils/Constants';
 
 const Head = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+  const [suggestionBox, setSuggestionBox] = useState(false);
   const dispatch = useDispatch();
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      getSearchSuggestions();
+    }, 200);
+    return () => {
+      clearTimeout(timer);
+    };
+    // eslint-disable-next-line
+  }, [searchQuery]);
+
+  const getSearchSuggestions = async () => {
+    const data = await fetch(YOUTUBE_SEARCH_SUGGESTION + searchQuery);
+    const json = await data?.json();
+    setSuggestions(json[1]);
   };
   return (
     <div className='grid grid-flow-col p-1 m-1 shadow-xl bg-white w-full items-center sticky z-50 top-0'>
@@ -24,16 +44,40 @@ const Head = () => {
         />
       </div>
 
-      <div className='col-span-10 flex justify-center'>
-        <input
-          className='w-2/5 border border-slate-300 rounded-l-full h-10 p-5 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500'
-          placeholder='Search'
-          type='text'
-        />
-        <button className='border p-2 px-4 bg-gray-100 hover:bg-gray-200 border-gray-300 rounded-r-full'>
-          Search
-        </button>
+      <div className='col-span-10 px-20'>
+        <div className=''>
+          <input
+            className='w-3/5 border border-slate-300 rounded-l-full h-10 p-5 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500'
+            placeholder='Search'
+            type='text'
+            onChange={(e) => setSearchQuery(e.target.value)}
+            value={searchQuery}
+            onFocus={() => setSuggestionBox(true)}
+            onBlur={() => setSuggestionBox(false)}
+          />
+          <button className='border p-2 px-4 bg-gray-100 hover:bg-gray-200 border-gray-300 rounded-r-full'>
+            Search
+          </button>
+        </div>
+
+        {suggestionBox && (
+          <div className='fixed bg-white w-[41%] shadow-2xl rounded-2xl py-2'>
+            <ul>
+              {suggestions.map((s) => (
+                <li
+                  key={s}
+                  onClick={(e) => setSearchQuery(e.target.value)}
+                  className='px-5 py-2 cursor-pointer hover:bg-gray-300'
+                >
+                  {s}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
+      <div></div>
+
       <div className='col-span-1'>
         <img
           className='h-12'
